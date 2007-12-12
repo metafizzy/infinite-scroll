213820f702575f6c9ffc8103173c40ab5f01f6d5
paulirish <paul.irish@gmail.com>
2007-12-12 07:12:29 +0000

    improving robustness of configurator

--- configurator.js
+++ configurator.js
@@ -1,6 +1,33 @@
-if (! jQis) var jQis = jQuery.noConflict();
-function log (str){ if (console && console.log) console.log(str); }
+/*
+WP-Infinite-Scroll configurator
+by: Paul Irish
+license: GPL
 
+TODO:
+ - Add some assertions:
+    - Check that all css selectors resolve to ONE element
+    - Check that the navigation element is "close" to the bottom
+ - fix silent error if content & post selectors are incorrect
+ - handle on frontend if there is no next link..
+*/
+
+if (! jQis) var jQis = jQuery.noConflict();
+
+if (! console) { var logdiv = jQis('<div/>').prependTo('body'); }
+
+function log (str){ 
+  if (console && console.log) console.log(str); 
+  else jQis('<div>'+str+'</div>').appendTo(logdiv);
+}
+function time (str,end){
+  if (!console) return;
+  
+  if (end === false)
+    console.timeEnd(str);
+  else
+    console.time(str);
+}
+    
 var config = {
   
   postcount : 10, // TODO: use get_option('posts_per_page') 
@@ -11,12 +38,10 @@ var config = {
   
   init : function(){
     
-    log('.');log('.');log('.');log('.');log('.');
-    
     var classes = {};
     
     // POST SELECTOR
-    console.time('posts');
+    time('posts');
     jQis('div').each(function(){  // document all the classes in use.
       var eachclass = jQis(this).attr('class');
       if (!eachclass) return true;
@@ -24,59 +49,69 @@ var config = {
       for (var i = 0; i < eachclass.length; i++){ classes[eachclass[i]] = true; }  
     });
     for (classx in classes){  // if any of classes appear as many times as the database says, and are siblings, we got a post selector!
-      if ( (jQis('div.'+classx).length == 10) && (jQis('div.'+classx).siblings('div.'+classx).length == 10) ){
-        if (config.postselector != null) log('We found more than one possible post selector. Problem!');
+      if ( (jQis('div.'+classx).length == config.postcount) && (jQis('div.'+classx).siblings('div.'+classx).length == config.postcount) ){
+        if (config.postselector != null) log('We found more than one possible post selector. First found was: ' + config.postselector);
         config.postselector = '.'+classx;
-        log('post selector is: ' +config.postselector);
+        log('Post selector is: ' +config.postselector);
         log(jQis(config.postselector));
+        log(jQis(config.postselector).length);
       }
     }
-    console.timeEnd('posts');
+    time('posts',false);
     
     //CONTAINER SELECTOR
     config.containerselector = jQis(config.postselector).parent().get(0);
     log('container is...');
     log(config.containerselector);
 
-    console.time('previous posts');
+    time('previous posts');
     
     
     //PREVIOUS POSTS 
-    config.nextlink = jQis('div[class~=pag],div[class~=nav],div[id~=pag],div[id~=nav]')
-      .find('a:contains("Older"),a:contains("Previous"),a:contains("Next"),a:contains("older"),a:contains("previous"),a:contains("next")');
-    if (! config.nextlink.length) // not found with above..
-      config.nextlink = jQis('a:contains("Older"),a:contains("Previous"),a:contains("Next"),a:contains("older"),a:contains("previous"),a:contains("next")'); // TODO: prompt user to pick.
-    if (! config.nextlink.length) // backup!! extra languages
-    config.nextlink = jQis('a:contains("anti")'); 
-    
-    log('prev posts is this guy...');
-    log(config.nextlink.get(0));
-    
-    console.timeEnd('previous posts');
+    config.nextlink = jQis('div[class~=pagingation],div[class~=navigation],div[id~=pagingation],div[id~=navigation]')
+      .find('a:contains("Older"),a:contains("Previous"),a:contains("Next"),a:contains("older"),a:contains("previous"),a:contains("next")')
+      .filter(':first');
+    if (! config.nextlink.length) { // not found with above..
+      config.nextlink = jQis('a:contains("Older"),a:contains("Previous"),a:contains("Next"),a:contains("older"),a:contains("previous"),a:contains("next")').filter(':first'); // TODO: prompt user to pick.
+    }
+    if (! config.nextlink.length){ // backup!! extra languages
+      config.nextlink = jQis('a:contains("anti")'); 
+    }
+    time('previous posts',false);
     
     
     
     //NAV LINKS SELECTOR
+    
+    time('navlinks');
     
     config.navigation = jQis('div[class~=pagingation],div[class~=navigation],div[id~=pagingation],div[id~=navigation]');    //check the obvious
     if (! config.navigation.length){ // not found with above..
 
       if (config.nextlink.parent().parent().get(0) == config.containerselector){
         // if parent parent is same as post container, its only the parent    
-        config.navigation = config.nextlink.parent().get(0);
+        config.navigation = config.nextlink.parent();
       }
       else if (config.nextlink.parent().get(0) == config.containerselector ){
         // what if there isnt a parent?? just an A hanging out...
-        config.navigation = config.nextlink.get(0); 
+        config.navigation = config.nextlink; 
       }
       else {
         // if there is no next link and NO navlinks.. then.. GUESS and ignore the hiding?
-        config.navigation = '';
+        config.navigation = config.nextlink.parent(); 
       }
 
     }
-    log(config.nextlink);
-  
+    
+    time('navlinks',false);
+    
+    log('nav links container:')
+    log(config.navigation.get(0));
+
+
+    log('prev posts is this guy...');
+    log(config.nextlink.get(0));
+    
   }, // end of init()
 
 
