64fec5d9fff5b9e7f742718ae39ac5ec96a15553
paulirish <paul.irish@gmail.com>
2009-11-02 21:31:45 +0000

    reorg for custom event triggering

--- jquery.infinitescroll.js
+++ jquery.infinitescroll.js
@@ -99,63 +99,70 @@
     function infscrSetup(path,opts,props,callback){
     
         if (props.isDuringAjax || props.isInvalidPage || props.isDone) return; 
-    
-    		if ( !isNearBottom(opts,props) ) return; 
-    		  
-    		// we dont want to fire the ajax multiple times
-    		props.isDuringAjax = true; 
-    		
-    		// show the loading message and hide the previous/next links
-    		props.loadingMsg.appendTo( opts.contentSelector ).show();
-    		$( opts.navSelector ).hide(); 
-    		
-    		// increment the URL bit. e.g. /page/3/
-    		props.currPage++;
-    		
-    		debug('heading into ajax',path);
-    		
-    		// if we're dealing with a table we can't use DIVs
-    		var box = $(opts.contentSelector).is('table') ? $('<tbody/>') : $('<div/>');  
-    		
-    		box
-    		  .attr('id','infscr-page-'+props.currPage)
-    		  .addClass('infscr-pages')
-    		  .appendTo( opts.contentSelector )
-    		  .load( path.join( props.currPage ) + ' ' + opts.itemSelector,null,function(){
-    		    
-    		        // if we've hit the last page...
-    		        if (props.isDone){ 
-                    showDoneMsg();
-        			      return false;    
-        			      
-  	            } else {
-  	              
-  	                // if it didn't return anything
-  	                if (box.children().length == 0){
-  	                  // fake an ajaxError so we can quit.
-  	                  $.event.trigger( "ajaxError", [{status:404}] ); 
-  	                } 
-  	                
-  	                // fadeout currently makes the <em>'d text ugly in IE6
-    		            props.loadingMsg.fadeOut('normal' ); 
-  
-    		            // smooth scroll to ease in the new content
-    		            if (opts.animate){ 
-      		            var scrollTo = $(window).scrollTop() + $('#infscr-loading').height() + opts.extraScrollPx + 'px';
-                      $('html,body').animate({scrollTop: scrollTo}, 800,function(){ props.isDuringAjax = false; }); 
-    		            }
-                    
-                    // pass in the new DOM element as context for the callback
-                    callback.call( box[0] );
-                    
-    		            if (!opts.animate) props.isDuringAjax = false; // once the call is done, we can allow it again.
-  	            }
-    		    }); // end of load()
-    			
-    		    
-      }  // end of infscrSetup()
+        
+        if ( !isNearBottom(opts,props) ) return; 
+        
+        $(document).trigger('load.infscr',kickOffAjax);
+                
+                
+    }  // end of infscrSetup()
           
   
+      
+    function kickOffAjax(path,opts,props,callback){
+        
+        // we dont want to fire the ajax multiple times
+        props.isDuringAjax = true; 
+        
+        // show the loading message and hide the previous/next links
+        props.loadingMsg.appendTo( opts.contentSelector ).show();
+        $( opts.navSelector ).hide(); 
+        
+        // increment the URL bit. e.g. /page/3/
+        props.currPage++;
+        
+        debug('heading into ajax',path);
+        
+        // if we're dealing with a table we can't use DIVs
+        var box = $(opts.contentSelector).is('table') ? $('<tbody/>') : $('<div/>');  
+        
+        box
+          .attr('id','infscr-page-'+props.currPage)
+          .addClass('infscr-pages')
+          .appendTo( opts.contentSelector )
+          .load( path.join( props.currPage ) + ' ' + opts.itemSelector,null,loadCallback); 
+        
+    }
+    
+    function loadCallback(){
+        // if we've hit the last page...
+        if (props.isDone){ 
+            showDoneMsg();
+            return false;    
+              
+        } else {
+          
+            // if it didn't return anything
+            if (box.children().length == 0){
+              // fake an ajaxError so we can quit.
+              $.event.trigger( "ajaxError", [{status:404}] ); 
+            } 
+            
+            // fadeout currently makes the <em>'d text ugly in IE6
+            props.loadingMsg.fadeOut('normal' ); 
+
+            // smooth scroll to ease in the new content
+            if (opts.animate){ 
+                var scrollTo = $(window).scrollTop() + $('#infscr-loading').height() + opts.extraScrollPx + 'px';
+                $('html,body').animate({scrollTop: scrollTo}, 800,function(){ props.isDuringAjax = false; }); 
+            }
+        
+            // pass in the new DOM element as context for the callback
+            callback.call( box[0] );
+        
+            if (!opts.animate) props.isDuringAjax = false; // once the call is done, we can allow it again.
+        }
+    }
     
       
     // lets get started.
@@ -198,7 +205,7 @@
                                   opts.loadingImg+'" /><div>'+opts.loadingText+'</div></div>');    
      // preload the image
     (new Image()).src    = opts.loadingImg;
-  		      
+              
 
   
     // set up our bindings
@@ -218,6 +225,7 @@
       .bind('scroll.infscr', function(){ infscrSetup(path,opts,props,callback); } )
       .trigger('scroll.infscr'); // trigger the event, in case it's a short page
     
+    $(document).bind('load.infscr',kickOffAjax);
     
     return this;
   
