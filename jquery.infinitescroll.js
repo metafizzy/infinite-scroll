2fdbade0d7f84340fa66fc39021a9d9ba81c01fc
Dayson Pais <dayson@epicwhale.org>
2012-06-09 16:40:54 +0530

    Rolled back the "Fixed validation to confirm that all selectors return results (not just the first one that randomly came up in the for...in)."
    This was causing the plugin to stop working and there is absolutely no output being returned in the console. It is working fine prior to this version: https://github.com/paulirish/infinite-scroll/commit/8ba7875dec22e76d70a70f2f0ef55fa068404d69

--- jquery.infinitescroll.js
+++ jquery.infinitescroll.js
@@ -443,6 +443,7 @@
                     this._debug('Your ' + key + ' found no elements.');
                     return false;
                 }
+                return true;
             }
             
             return true;
