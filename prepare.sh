f3949e83c8d9e2ab13c5eab39b863d518b4c87d9
Glenn Nelson <glenn@hexcoder.us>
2012-11-03 21:42:12 -0400

    Added script to automate prepartion before commit & updated WordPress plugin scripts

---
+++ prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Automate the prepartion of all scripts from the main developement source
+# Requires the existence of YUI Compressor & an alias as `yui`
+#
+# Assuming the alias exists on the user's account, invoke by `. ./prepare.sh`
+#
+
+# Minifiy the script
+yui jquery.infinitescroll.js -o jquery.infinitescroll.min.js
+
+# Copy scripts into the WordPress plugin
+cp jquery.infinitescroll.js ./wordpress-plugin/js/front-end/jquery.infinitescroll.dev.js
+cp jquery.infinitescroll.min.js ./wordpress-plugin/js/front-end/jquery.infinitescroll.js
+
