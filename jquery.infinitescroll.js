ecb493d6ed18059f59c0da382fa33fb6bc7400af
Haydar Kulekci <haydar.kulekci@reformo.net>
2012-12-21 16:14:34 +0200

    default maxPage is changed and destroy() method is used instead of isDestroy control

--- jquery.infinitescroll.js
+++ jquery.infinitescroll.js
@@ -62,7 +62,7 @@
         pixelsFromNavToBottom: undefined,
         path: undefined, // Either parts of a URL as an array (e.g. ["/page/", "/"] or a function that takes in the page number and returns a URL
 		prefill: false, // When the document is smaller than the window, load data until the document is larger or links are exhausted
-        maxPage:0 // to manually control maximum page (when maxPage is 0, maximum page limitation is not work)
+        maxPage: undefined // to manually control maximum page (when maxPage is undefined, maximum page limitation is not work)
 	};
 
     $.infinitescroll.prototype = {
@@ -535,9 +535,8 @@
 			opts.state.currPage++;
 
             // Manually control maximum page 
-            if ( opts.maxPage != 0 && opts.state.currPage > opts.maxPage ){
-                opts.state.isDestroyed = true;
-                instance._error('end');
+            if ( opts.maxPage != undefined && opts.state.currPage > opts.maxPage ){
+                this.destroy();
                 return;
             }
 
