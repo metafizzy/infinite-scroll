44cea8214e46daa0c110164b38068f4ecd2c2320
Glenn Nelson <glenn@hexcoder.us>
2013-04-20 15:35:22 -0400

    Added instance-safe event (de)registering, URL creation, additional skeleton

--- jquery.infinite-scroll.js
+++ jquery.infinite-scroll.js
@@ -3,46 +3,92 @@
 (function($, window, undefined) {
 	"use strict";
 
-	var EVENT_NAMESPACE = ".infinite-scroll";
+	var INSTANCES = 0;
 
-	var EVENTS = {
-		// Triggered events
-		loadingStart: "loading" + EVENT_NAMESPACE,
-		loadingComplete: "loading" + EVENT_NAMESPACE,
-		finished: "finished" + EVENT_NAMESPACE
-
-		// Subscribed events
-	};
+	var DATA_KEY = "infiniteScroll";
 
 	$.infiniteScroll = function(options, element) {
 		this.$element = $(element);
+		this.$window = $(window);
 		this.options = $.extend({}, this.defaults, options);
+
+		this._init();
 	};
 
 	$.defaults = {
+		scrollSelector: "",
 		navigationSelector: "",
 		nextSelector: "",
 		itemSelector: "",
 		debug: false,
-		path: undefined
+		page: 1,
+		path: undefined,
+		fill: true
 
 		// TODO: Rethink plugin ("behavior") loader to allow for multiple plugins
 	};
 
 	$.infiniteScroll.prototype = {
+		state: {
+			isPaused: false,
+			isLoading: false
+		},
+
 		// Private methods
 
 		_init: function() {
-			this.pageNumber = 1;
+			this.instanceId = INSTANCES++;
+			this.page = this.options.page;
+			this.path = this.options.path;
+			this.fill = this.options.fill;
+
+			this.$scroll = $(this.options.scrollSelector);
+			this.$navigation = $(this.options.navigationSelector);
+			this.$nextSelector = $(this.options.nextSelector);
+
+			// If the path creation isn't overridden, create a path array by parsing the URL provided by the next selector
+			var nextUrl = this.$nextSelector.attr("href");
+			this.path = this.path || this._parsePath(nextUrl, this.page);
+
+			// Setup a namespacing system for events so they can be added and removed without breaking other instances
+			var EVENT_NAMESPACE = ".infinite-scroll" + this.instanceId;
+
+			this.EVENTS = {
+				// Triggered events
+				LOADING_START: "loading" + EVENT_NAMESPACE,
+				LOADING_COMPLETE: "loading" + EVENT_NAMESPACE,
+				FINISHED: "finished" + EVENT_NAMESPACE,
+
+				// Subscribed events
+				RESIZE: "resize" + EVENT_NAMESPACE
+			};
+
+			if (this.fill) {
+				this.$window.on(this.EVENTS.RESIZE, $.proxy(this._resize, this));
+			}
 		},
 
-		_getPath: function(pageNumber) {
+		/**
+		 * Parses a URL into component parts that can be used to create URLs to load other pages
+		 *
+		 * @param path {String}
+		 * @param page {Number}
+		 * @returns {Array}
+		 * @private
+		 */
+		_parsePath: function(path, page) {
 			// TODO: Create heuristics similar to what is in Infinite-Scroll 
-			return ["/page/", "?query=value"];
+			return path.split(page);
 		},
 
 		_getUrl: function() {
-			return this._getPath(this.pageNumber).join(this.pageNumber);
+			if ($.isFunction(this.path)) {
+				return this.path(this.page);
+			} else if ($.isArray(this.path)) {
+				return this.path.join(this.page.toString());
+			} else if (typeof this.path === "string") {
+				return this.path;
+			}
 		},
 
 		_getPage: function(url, data, dataType) {
@@ -59,28 +105,54 @@
 			});
 		},
 
+		_shouldLoad: function() {
+			return true;
+		},
+
 		_debug: function() {
 			// TODO: Write universal (that is IE8+) debugging method
 		},
 
+		/**
+		 * Loads additional content into the scrolling area's viewport if it does not have a scrollbar present
+		 *
+		 * @event
+		 * @private
+		 */
+		_resize: function() {
+			// TODO: Load additional content if `this.$scroll` is not full
+		},
+
 		// Public methods
 
+		/**
+		 * Removes all event handlers and data for the plugin instance
+		 */
 		destroy: function() {
-
+			this.$element.removeData(DATA_KEY);
+			this.$window.off(this.EVENTS.RESIZE);
 		},
 
+		/**
+		 * Stops the plugin from loading any new pages, or otherwise altering any plugin state. The plugin can be
+		 * @see {@link resume}
+		 */
 		pause: function() {
-
+			this.state.isPaused = true;
 		},
 
+		/**
+		 * Allows the plugin to continue operating after having been paused
+		 * @see {@link pause}
+		 */
 		resume: function() {
-
+			this.state.isPaused = false;
 		}
 	};
 
 	$.fn.infiniteScroll = function(options) {
 		var type = typeof options;
-		var instance = this.data("infiniteScroll");
+		var instance = this.data(DATA_KEY);
 
 		switch (type) {
 			case "string":
@@ -91,6 +163,7 @@
 			case "object":
 				if (!instance) {
 					instance = new $.infiniteScroll(options, this);
+					this.data(DATA_KEY, instance);
 				}
 				break;
 		}
