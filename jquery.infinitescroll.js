8f813cccb30bc8fa8936fe92878184ff8574c807
bill2004158 <bill2004158@sina.com>
2013-07-06 19:51:52 +0800

    Update jquery.infinitescroll.js
    I think nextSelector is not required if path is provided?

--- jquery.infinitescroll.js
+++ jquery.infinitescroll.js
@@ -122,7 +122,7 @@
             var path = $(opts.nextSelector).attr('href');
             if (!path) {
                 this._debug('Navigation selector not found');
-                return false;
+                //return false;
             }
 
             // Set the path to be a relative URL from root.
