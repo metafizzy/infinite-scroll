d2b25d9df8bb02c13dac53405bbdfb2f9b2cc896
David DeSandro <desandrocodes@gmail.com>
2018-01-12 13:33:55 -0500

    👷 build v3.0.3; ⬆️ update imagesLoaded v4.1.4
    for #671
    
    📅 2018

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 /*!
- * Infinite Scroll v3.0.2
+ * Infinite Scroll v3.0.3
  * Automatically add next page
  *
  * Licensed GPLv3 for open source use
  * or Infinite Scroll Commercial License for commercial use
  *
  * https://infinite-scroll.com
- * Copyright 2017 Metafizzy
+ * Copyright 2018 Metafizzy
  */
 
 ( function( window, factory ) {
